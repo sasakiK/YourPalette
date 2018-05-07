9582489ba2a1d22d516b3cf60e63caf503d54248
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-05-08 00:44:23 +0900

    modify css

--- app/app.py
+++ app/app.py
@@ -5,14 +5,38 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
+from flask import send_from_directory
+import os
+
 from PIL import Image, ImageDraw
 
 app = dash.Dash()
-
+app.title='Your Palette'
 app.scripts.config.serve_locally = True
 
 
+# app.head = [('''
+#     <style type="text/css">
+#     html, body {
+#       height: 100%;
+#       background-color: #003F98 !important;
+#       font-size: "30px";
+#     }
+#     </style>
+#     '''),
+#     html.Title(path)
+# ]
+#
+# app.footer = [
+#     html.Script(type='text/javascript', children='alert("hello world")')
+# ]
+
+
 app.layout = html.Div([
+    html.Link(
+        rel='stylesheet',
+        href='/static/stylesheet.css'
+    ),
 
     html.H1(['Make your own palette'],
     style={'font-size': '6vw', 'margin-left': '11%', 'margin-bottom': '7%', 'margin-top': '10%'}),
@@ -58,7 +82,7 @@ def parse_contents(contents, filename, date):
 
     return html.Div([
         html.H5([filename], style={'margin-top': '8%'}),
-        html.H6(datetime.datetime.fromtimestamp(date)),
+        html.H5(datetime.datetime.fromtimestamp(date)),
 
         # HTML images accept base64 encoded strings in the same format
         # that is supplied by the upload
@@ -72,18 +96,25 @@ def parse_contents(contents, filename, date):
         html.Hr(),
     ])
 
-# def get_colors(infile, numcolors=10, swatchsize=20, resize=150):
+def get_colors(infile, numcolors=10, swatchsize=20, resize=150):
+
+    image = Image.open("DASH.png")
+    image = image.resize((resize, resize))
+    result = image.convert('P', palette=Image.ADAPTIVE, colors=numcolors)
+    result.putalpha(0)
+    colors = result.getcolors(resize*resize)
+
+    # Save colors to file
+    pal = Image.new('RGB', (swatchsize*numcolors, swatchsize))
+
+    return pal
+
+# def head():
+#     path = request.path
+#     return html.Title(path)
 #
-#         image = Image.open("DASH.png")
-#         image = image.resize((resize, resize))
-#         result = image.convert('P', palette=Image.ADAPTIVE, colors=numcolors)
-#         result.putalpha(0)
-#         colors = result.getcolors(resize*resize)
-#
-#         # Save colors to file
-#         pal = Image.new('RGB', (swatchsize*numcolors, swatchsize))
-#
-#         return pal
+# app.head = head
+
 
 @app.callback(Output('output-image-upload', 'children'),
               [Input('upload-image', 'contents'),
@@ -97,6 +128,11 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
         return children
 
 
+
+@app.server.route('/static/<path:path>')
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), 'static')
+    return send_from_directory(static_folder, path)
 
 external_css = [
     # dash stylesheet
